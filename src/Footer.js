f5c43c7561cbe2ac487a6ee9c8abdd50d963e210
Jeremy Cleland <jeremy@clelandco.com>
2023-01-23 19:43:38 -0500

    add about us nav-link /update README

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import { Navbar, Link, NavItem } from "react-bootstrap";
 
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Navbar.Brand>Code Fellows</Navbar.Brand>-
+        <NavItem>
+          <Link to='/about' className='nav-link'>
+            About
+          </Link>
+        </NavItem>
       </Navbar>
-    )
+    );
   }
 }
 
