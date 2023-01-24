852fad46277c131f83edcb9e72e2765a7ec95ab8
Jeremy Cleland <jeremy@clelandco.com>
2023-01-23 19:47:29 -0500

    fix bug

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Navbar, Link, NavItem } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 
 class Footer extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
         <Navbar.Brand>Code Fellows</Navbar.Brand>-
-        <NavItem>
-          <Link to='/about' className='nav-link'>
-            About
-          </Link>
-        </NavItem>
       </Navbar>
     );
   }
