4379be12dceca5deaae9807a1a58ef3f93573dd7
Jeremy Cleland <jeremy@clelandco.com>
2023-01-23 19:30:36 -0500

    add about us page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,20 @@
 import { Component } from "react";
 
 class Profile extends Component {
-
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <div>
+        <h2>Our Team</h2>
+        <p>
+          Our team is made up of 2 developers who are passionate about coding
+          and learning new things.
+        </p>
+        <p>Marcus Bynoe</p>
+        <p>Jeremy Cleland</p>
+      </div>
+    );
   }
-};
+}
 
 export default Profile;
