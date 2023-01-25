41a5911cb78a629c4fa904f8d2f65aa0d19e6405
Jeremy Cleland <jeremy@clelandco.com>
2023-01-24 20:54:01 -0500

    fix bug bookform

--- src/Components/BookFormModal/BookFormModal.js
+++ src/Components/BookFormModal/BookFormModal.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./BookFormModal.css";
 import { Modal, Button } from "react-bootstrap";
 
-export class BookFormModal extends React.Component {
+class BookFormModal extends React.Component {
   render() {
     return (
       <Modal
         className='modal'
+        style={{ textAlign: "center" }}
         show={this.props.handleShowModal}
         onHide={this.props.handleCloseModal}
       >
@@ -14,10 +15,15 @@ export class BookFormModal extends React.Component {
         <form>
           <label htmlFor='title'>Title</label>
           <input type='text' placeholder='Title' name='title' id='title' />
-          <label htmlFor='title'>Description</label>
-          <input type='text' placeholder='Title' name='title' id='title' />
-          <label htmlFor='title'>Status</label>
-          <input type='text' placeholder='Title' name='title' id='title' />
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            placeholder='Description'
+            name='description'
+            id='description'
+          />
+          <label htmlFor='status'>Status</label>
+          <input type='text' placeholder='Status' name='status' id='status' />
           <Button type='submit'>Add new</Button>
         </form>
       </Modal>
